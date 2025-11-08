9c65580bcf61358560b489378aafed06da096524
Thero16 <juan_pablo9687@hotmail.com>
2025-11-08 09:27:38 -0500

    first commit

---
+++ config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Clonar repo de GitHub
+git clone 
+
+cd 
+
+#Creacion entorno virtual de python
+python3 -m venv .venv
+source .venv/bin/activate
+
+#Descargar librerias
+pip install -r requirements.txt
+
+
